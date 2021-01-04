3e9c8c48d07f8511bab416a9c299f8f26e4dc56e
willmasse <willmasse@gmail.com>
2021-01-04 16:09:32 -0500

    Viz Updates

--- mm-site/apps/mm_21_1.py
+++ mm-site/apps/mm_21_1.py
@@ -1,8 +1,10 @@
+import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
+import numpy as np
 import requests
 import plotly.express as px
 
@@ -11,19 +13,169 @@ from navbar import Navbar
 
 df = pd.read_excel('assets/RailsToTrails_National Count Data_week.xlsx')
 df = df.dropna(axis=0, subset=["Week"])
-df["BikesPerPed"] = (df['Bikes (14 counters)']/df['Pedestrians (14 counters)'])-1
-fig = px.line(df, x="Timeframe", y="Counts (31 counters)", color="Year")
-fig2 = px.line(df, x="Timeframe", y="BikesPerPed", color="Year")
+df["Year"] = df['Year'].astype('int').astype("str")
+df["Bike Week"] = df["Bikes (14 counters)"] > df["Pedestrians (14 counters)"]
+
+
+def agg_func(row):
+    if row["Bikes (14 counters)"] >= row["Pedestrians (14 counters)"]:
+        return (row['Bikes (14 counters)']/row['Pedestrians (14 counters)'])-1
+    else:
+        return 0-((row['Pedestrians (14 counters)']/row['Bikes (14 counters)'])-1)
+
+def about_func(row):
+    if row["Bikes (14 counters)"] >= row["Pedestrians (14 counters)"]:
+        return "Bikers outnumbered Walkers " + str(round(row['Bikes (14 counters)']/row['Pedestrians (14 counters)'], 2)) + " to 1."
+    elif row["Bikes (14 counters)"] < row["Pedestrians (14 counters)"]:
+        return "Bikers outnumbered Walkers " + str(round(row['Pedestrians (14 counters)']/row['Bikes (14 counters)'], 2)) + " to 1."
+    else:
+        return "None"
+
+
+df["Ratio"] = df.apply(agg_func, axis=1)
+df["About"] = df.apply(about_func, axis=1)
+
+
+fig = px.line(df, x="Timeframe", y="Counts (31 counters)", color="Year", labels ={
+    "Counts (31 counters)":"Trail Users",
+    "Timeframe":"Week"
+})
+
+
+fig_2_text = html.Div([
+    dbc.Button(
+        html.Div([
+            html.H5("Walkers outnumber bikers..."),
+            html.P("In 58 weeks out of the 92 observed walkers outnumbered bikers.")
+        ]),
+        id="button-1", className="mb-2 mr-3 mt-3"),
+    dbc.Button(
+        html.Div([
+            html.H5("In 2019.."),
+            html.P("In 2019 bikers only outnumberd walkers on 13 out of the 46 weeks observed.")
+        ]),
+        id="button-2", className="mt-2 mr-3"),
+    dbc.Button(
+        html.Div([
+            html.H5("In 2020 bikers are catching up!"),
+            html.P("In 2020 bikers outnumbered walkers in 21 out of the 46 weeks observed.")
+        ]),
+        id="button-3", className="mt-3 mr-3"
+    )
+])
+
+
 nav = Navbar()
 
 body = html.Div([
     dbc.Row([
-        dbc.Col(width=4),
+        dbc.Col(width=6),
         dbc.Col(
-            dcc.Graph(figure=fig2, id="line-chart"),
-            width=8
+            dcc.Graph(figure=fig, id="line-chart"),
+            width=6
         ),
-    ])
+    ]),
+    dbc.Row([
+        dbc.Col(
+            dcc.Graph(id="graph2", config={
+        'displayModeBar': False
+    }),
+            width=8),
+        dbc.Col(fig_2_text,width=4)
+    ], className="mt-4"),
+    
+
 ])
 
 layout = html.Div([nav, body])
+
+@app.callback(
+    Output("graph2", 'figure'),
+    Input('button-1', 'n_clicks'),
+    Input('button-2', "n_clicks"),
+    Input('button-3', "n_clicks")
+)
+def display(button1, button2, button3):
+    ctx = dash.callback_context
+
+    fig2 = px.scatter(df, x="Ratio", y="Timeframe", color="Year", height=800, hover_data={
+        "Year":True,
+        "Timeframe":True,
+        "Ratio":False,
+        "About":True
+    })
+    fig2.update_yaxes(dtick=2, autorange="reversed")
+    fig2.update_layout(shapes=[dict(
+        type="line",
+        yref='paper', y0=0, y1=1,
+        xref="x", x0=0, x1=0,
+        line=dict(color="Black", width=3, dash="dash")
+    )])
+    fig2.update_xaxes(
+        tickmode="array",
+        tickvals=[-2, -1, -.5, 0, .5, 1 ,2],
+        ticktext=["3/1", "2/1", "1.5/1", "1/1", "1.5/1", "2/1", "3/1"],
+        range=[-3,3],
+        side="top"
+    )
+    fig2.add_annotation(
+        x=1,
+        y=-1,
+        text="More Bikers per Walkers",
+        showarrow=False
+
+    )
+    fig2.add_annotation(
+        x=-1,
+        y=-1,
+        text="More Walkers per Bikers",
+        showarrow=False
+
+    )
+
+    fig2.update_traces(marker=dict(
+    size=12,
+    line=dict(
+        width=2,
+        color="DarkSlateGrey")),
+    selector=dict(mode="markers")
+    )
+
+    for week in df["Timeframe"].unique():
+        row_2019 = df.loc[(df["Timeframe"]==week) & (df["Year"]=="2019")]
+        row_2020 = df.loc[(df["Timeframe"]==week) & (df["Year"]=="2020")]
+        if not pd.isna(row_2019["Ratio"].iloc[0]):
+            fig2.add_shape(
+                type="line",
+                x0=row_2019["Ratio"].iloc[0], y0=week,
+                x1=row_2020["Ratio"].iloc[0] , y1=week,
+                line_color="#cccccc"
+            )
+
+    if not ctx.triggered:
+        pass
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split(".")[0]
+        if button_id == "button-1":
+            t_df = df[df["Ratio"] < 0]
+        elif button_id == "button-2":
+            t_df = df[(df["Ratio"]>0)&(df["Year"]=="2019")]
+        elif button_id == "button-3":
+            t_df = df[(df["Ratio"]>0)&(df["Year"]=="2020")]
+        fig2.add_trace(px.scatter(t_df, x="Ratio", y="Timeframe", height=800, hover_data={
+        "Year":True,
+        "Timeframe":True,
+        "Ratio":False,
+        "About":True}).update_traces(marker=dict(
+                size=12,
+                color="rgba(135, 206, 250, 0)",
+                line=dict(
+                    width=4,
+                    color="Black")),
+                selector=dict(mode="markers")
+                ).data[0])
+                
+    
+    return fig2
+
+
